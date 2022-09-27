fb02653084560308e155a9ec84e835b92fd352f4
Nada Hasni <nadasthing@gmail.com>
2022-09-27 12:11:12 +0700

    add home template butterfly bootstrap

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\HomeController;
 use App\Http\Controllers\ManagementUserController;
 use Illuminate\Support\Facades\Route;
 
@@ -15,5 +16,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', [ManagementUserController::class, 'index']);
+
+Route::resource('home', HomeController::class);
 
 Route::resource('user', ManagementUserController::class);
