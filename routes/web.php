3dd205fc96ff7ee8f7fffd55fcffb492e50a8e91
Nada Hasni <nadasthing@gmail.com>
2022-10-02 16:17:29 +0700

    add dashboard backend with template

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\DashboardController;
 use App\Http\Controllers\HomeController;
 use App\Http\Controllers\ManagementUserController;
 use Illuminate\Support\Facades\Route;
@@ -18,5 +19,6 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', [ManagementUserController::class, 'index']);
 
 Route::resource('home', HomeController::class);
+Route::resource('dashboard', DashboardController::class);
 
 Route::resource('user', ManagementUserController::class);
